3268a8d84d52274580a900997f59ad90f25bd28a
fagat <fagathe77@gmail.com>
2021-05-11 17:48:01 +0200

    Issue #10 Api user profile

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,11 +4,10 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-
-// use FOS\RestBundle\Controller\Annotations as Rest;
-// use FOS\RestBundle\Controller\Annotations\View;
-// use Symfony\Component\HttpFoundation\Request;
-// use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends AbstractFOSRestController
 {
@@ -23,16 +22,16 @@ class AuthController extends AbstractFOSRestController
         $this->repository = $repository;
     }
 
-    // /**
-    //  * @Rest\Get(
-    //  *  "/api/auth/profile",
-    //  *  name="api_auth_profile",
-    //  * )
-    //  * @View(serializerGroups={"user:read"})
-    //  */
-    // public function profile(Request $request)
-    // {
-    //     return $this->view($this->getUser(), Response::HTTP_OK);
-    // }
+    /**
+     * @Rest\Get(
+     *  "/api/auth/profile",
+     *  name="api_auth_profile",
+     * )
+     * @View(serializerGroups={"user:read"})
+     */
+    public function profile(Request $request)
+    {
+        return $this->view($this->getUser(), Response::HTTP_OK);
+    }
 
 }
