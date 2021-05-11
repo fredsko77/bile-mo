90b668abcffc22f5b3f124fd543b0614c5caabfc
fagat <fagathe77@gmail.com>
2021-05-11 18:18:52 +0200

    Issue #8 client_user delete

--- src/Controller/ClientUserController.php
+++ src/Controller/ClientUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ClientUser;
 use App\Repository\ClientUserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\View;
@@ -18,9 +19,15 @@ class ClientUserController extends AbstractFOSRestController
      */
     private $repository;
 
-    public function __construct(ClientUserRepository $repository)
+    /**
+     * @var EntityManagerInterface $manager
+     */
+    private $manager;
+
+    public function __construct(ClientUserRepository $repository, EntityManagerInterface $manager)
     {
         $this->repository = $repository;
+        $this->manager = $manager;
     }
 
     /**
@@ -41,8 +48,13 @@ class ClientUserController extends AbstractFOSRestController
         return $this->view('Unauthorized', Response::HTTP_UNAUTHORIZED);
     }
 
+    /**
+     * @Rest\Post()
+     * @View
+     */
     public function create(Request $request)
     {
+        // TO DO create a new User Client
         return $this->view([], Response::HTTP_OK);
     }
 
@@ -53,8 +65,30 @@ class ClientUserController extends AbstractFOSRestController
      * )
      * @View(serializerGroups={"client:list"})
      */
-    function list() {
+    public function listClient()
+    {
         return $this->view($this->getUser()->getClientUsers());
+    }
+
+    /**
+     * @Rest\Delete(
+     *  "/api/client_user/{ref}",
+     *  name="api_client_user_delete",
+     *  requirements={"ref"="^([\w]+)_([\w]+)-([\d]+)$"}
+     * )
+     * @View(serializerGroups={"client:read"})
+     */
+    public function delete(ClientUser $client)
+    {
+        if ($client->getUser()->getId() === $this->getUser()->getId()) {
+
+            $this->manager->remove($client);
+            $this->manager->flush();
+
+            return $this->view('Content Deleted', Response::HTTP_OK);
+        }
+
+        return $this->view('Unauthorized', Response::HTTP_UNAUTHORIZED);
     }
 
 }
